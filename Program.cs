4b724246a45d196270fe7c21b6efbbded9dc9c00
bruno0011 <brunosouzviana@hotmail.com>
2022-04-08 21:19:49 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int numerodigitado;
+
+Console.Write("Qual a sua idade:");
+numerodigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numerodigitado > 80)
+{
+Console.WriteLine("Você não pode ser doador de sangue.");
+}
+
+else if (numerodigitado < 15 )
+{
+Console.WriteLine("Você não pode ser doador de sangue.");
+}
+else if (numerodigitado > 15)
+{
+Console.WriteLine("Você pode ser doador de sangue.");
+}
